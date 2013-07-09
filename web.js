83d63e504dce06cf306ea0b00e1a393ce7c999ac
Jason Willis <brandr2k@gmail.com>
2013-07-09 01:57:03 +0000

    2nd attempt at file read

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8',0,fs.readFileSync('index.html'));
+  var indexbuffer = new Buffer();
+  indexbuffer = fs.readFileSync('index.html');
+  response.send(indexbuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
